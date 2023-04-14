2bd91771ef418b3af5f1e4ed2deeb5fa1d0fe47e
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-04-15 01:46:52 +0300

    stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.head = null;
+    this.top = null;
+  }
+  push(/* element */ element) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (this.head === this.top) {
+      this.top = {
+        data: element,
+        prev: this.head,
+      };
+    } else {
+      this.top = {
+        data: element,
+        prev: this.top,
+      };
+    }
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let out = this.top.data;
+    this.top = this.top.prev;
+    return out;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.top ? this.top.data : undefined;
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
